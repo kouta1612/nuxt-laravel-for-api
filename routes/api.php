b040d0a64b12d67d89d9dd6abd3a4f87bd196232
kouta1612 <kouta1612world69@gmail.com>
2021-01-31 19:28:56 +0900

    install breeze

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:api')->post('/register', [RegisteredUserController::class, 'store']);
+Route::middleware('auth:api')->post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::get('/json', function (Request $request) {
+    return response('test')->json();
 });
